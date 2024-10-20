4e5270cb5782ac751cc4ddea743f4f824d9a6ed7
Leo Rand <leopold.rand@gmail.com>
2024-10-20 18:25:08 +0530

    Built form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 
 function TodoForm() {
+	const [value, setValue] = useState("");
+
+	function handleSubmit(e) {
+		e.preventDefault;
+	}
+
 	return (
-		<form className="TodoForm">
+		<form className="TodoForm" onSubmit={handleSubmit}>
 			<input
 				type="text"
 				className="todo-input"
 				placeholder="Type your task here..."
+				onChange={(e) => setValue(e.target.value)}
 			/>
 			<button className="todo-btn" type="submit">
 				Enter Task
